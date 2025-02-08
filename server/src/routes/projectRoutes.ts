1504684752d5231abb08647f07e55a339e7a40ec
Rahul kumar <rahulkumarsah269@gmail.com>
2025-02-08 13:16:55 +0530

    init

---
+++ server/src/routes/projectRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { createProject, getProjects } from "../controllers/projectController";
+
+const router = Router();
+
+router.get("/", getProjects);
+router.post("/", createProject);
+
+export default router;
+
+ 
